41408c90bc927236c7902a0ce7026eb45392c93e
tarmacpro <jimmysyim@gmail.com>
2021-10-20 17:58:52 -0700

    uploading video preview

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+function onmouseover() {
+    var x = document.getElementById("#watherfall");
+    play(x);
+}
+
+function onmouseout() {
+    var x = document.getElementById("#watherfall");
+}
